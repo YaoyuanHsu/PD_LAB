328b6148f22954ed0428f18ac33be68d52ccc838
YaoyuanHsu <33758678+YaoyuanHsu@users.noreply.github.com>
2017-12-03 17:47:24 +0800

    change_name

--- Lab8/Blackjack.h
+++
@@ -1,7 +0,0 @@
-int WelcomeMessage();
-void InputName(char *name);
-void ShuffleCards(int *card);
-int Face(int* card);
-int MenuSelection(char * inputchar);
-int* DealCards(int* cardPosition, int* dP1, int* pP1, int* dP2, int* pP2, int* ds, int* ps);
-void Point(int* face, int* sum);
